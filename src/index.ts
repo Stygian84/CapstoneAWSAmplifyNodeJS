2d6ea989d8a9c3de238e3a74af31b9b3e9a3ce8d
Stygian84 <nicholasgandhi84@gmail.com>
2024-02-08 09:22:11 +0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,162 @@
-// src/index.ts
-import express from 'express';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { Pool } from 'pg';
+import https from 'https';
 
-const app: express.Application = express();
-const port = 3000;
+import dotenv from 'dotenv';
+dotenv.config();
 
-app.use(express.text());
+const port = process.env.PORT || 3001;
+const app = express();
+app.use(cors());
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT)
+});
+
+app.get("/api/data/:tableName", async (req: Request, res: Response) => {
+  const { tableName } = req.params;
+
+  try {
+    const result = await pool.query(`SELECT * FROM public."${tableName}"`);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/api/query/:sqlQuery", async (req: Request, res: Response) => {
+  const { sqlQuery } = req.params;
+
+  try {
+    const result = await pool.query(sqlQuery);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/api/row", async (req: Request, res: Response) => {
+  try {
+    const query = {
+      text: `SELECT RowID, Status
+      FROM public."RowData"
+      ORDER BY RowID ASC;
+      `,
+    };
+
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/api/status", async (req: Request, res: Response) => {
+  try {
+    const query = {
+      text: `SELECT *
+      FROM public."RowData"
+      ORDER BY RowID ASC;
+      `,
+    };
+
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/api/status/:rowId", async (req: Request, res: Response) => {
+  try {
+    const { rowId } = req.params;
+    const query = {
+      text: `SELECT *
+            FROM public."RowData"
+            WHERE RowID = $1
+            ORDER BY RowID ASC`,
+      values: [rowId],
+    };
+
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/api/plant", async (req: Request, res: Response) => {
+  try {
+    const query = {
+      text: `SELECT *
+      FROM public."PlantData"
+      ORDER BY RowID ASC;
+      `,
+    };
+
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/api/plant/:rowId", async (req: Request, res: Response) => {
+  try {
+    const { rowId } = req.params;
+    const query = {
+      text: `SELECT *
+          FROM public."PlantData" pd
+          JOIN (
+          SELECT PlantID, MAX(Timestamp) AS latest_timestamp
+          FROM public."PlantData"
+          WHERE RowID = $1
+          GROUP BY PlantID
+          ) AS latest ON pd.PlantID = latest.PlantID AND pd.Timestamp = latest.latest_timestamp
+          ORDER BY pd.PlantID ASC;
+      
+      `,
+      values: [rowId],
+    };
+
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/api/plant/:rowId/:plantId/:property", async (req: Request, res: Response) => {
+  try {
+    const { rowId, plantId, property } = req.params;
+    const query = {
+      text: `SELECT timestamp, ${property}
+      FROM public."PlantData"
+      WHERE RowID = $1 AND PlantID = $2
+      ORDER BY timestamp ASC`,
+      values: [rowId, plantId],
+    };
+
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`server is listening on ${port}`);
-});
-
-// Homepage
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.status(200).send("Hello World!");
-});
-
-// GET
-app.get('/get', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-get-header", "get-header-value").send("get-response-from-compute");
-});
-
-//POST
-app.post('/post', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-post-header", "post-header-value").send(req.body.toString());
-});
-
-//PUT
-app.put('/put', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-put-header", "put-header-value").send(req.body.toString());
-});
-
-//PATCH
-app.patch('/patch', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-patch-header", "patch-header-value").send(req.body.toString());
-});
-
-// Delete
-app.delete('/delete', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-delete-header", "delete-header-value").send();
+  console.log(`Server is running on http://localhost:${port}`);
 });
